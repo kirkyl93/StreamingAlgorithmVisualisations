fdf90700a608868e46b8d1a0004e2065a59ae7f2
kirkyl93 <l.kirkham@hotmail.com>
2022-06-30 10:48:37 +0100

    Made big improvements to quality of the code. User can now enter B values in an array before program execution. This gives the user a lot more flexibility.

--- ApproximateCounting/src/main/java/counting/approximatecounting/ComparingMorrisCounterBValues.java
+++ ApproximateCounting/src/main/java/counting/approximatecounting/ComparingMorrisCounterBValues.java
@@ -15,18 +15,18 @@ public class ComparingMorrisCounterBValues extends Application {
     @Override
     public void start(Stage stage) {
 
+        // Set the b values for the lines
+        final double[] B_VALUES = {1.05, 1.2};
+
         // Set the number of counters used for finding the average. If we set this to 100, we run 100 Morris and
         // Approximate Counters simultaneously and take their average.
         final int NUMBER_OF_COUNTERS = 1;
 
         // Set the value to which our counters count to.
-        final int COUNT_TO_VALUE = 1000;
+        final long COUNT_TO_VALUE = 20L;
 
         // Set the number of updates made to our counters before refreshing the graph visualisation.
-        final int UPDATES_PER_FRAME = 1000;
-
-        //Set the number of lines added to the graph.
-        final int NUMBER_OF_LINES = 3;
+        final int UPDATES_PER_FRAME = 1;
 
         // Prepare line chart
         stage.setTitle("Comparing B values in Morris Counter");
@@ -41,25 +41,33 @@ public class ComparingMorrisCounterBValues extends Application {
 
         XYChart.Series<Number, Number> basicCounterLine = new XYChart.Series<>();
         basicCounterLine.setName("Basic Counter");
-        ArrayList<XYChart.Series<Number, Number>> morrisLines = new ArrayList<>();
+        LINE_CHART.getData().add(basicCounterLine);
+
+
+        ArrayList<XYChart.Series<Number,Number>> morrisLines = new ArrayList<>();
         ArrayList<XYChart.Series<Number, Number>> updateLines = new ArrayList<>();
-        ArrayList<ArrayList<MorrisCounter>> morrisCounters = new ArrayList<>();
-        for (int i = 0; i < NUMBER_OF_LINES; i++) {
+
+
+        for (int i = 0; i < B_VALUES.length; i++) {
             XYChart.Series<Number, Number> morrisLine = new XYChart.Series<>();
-            XYChart.Series<Number, Number> updateLine = new XYChart.Series<>();
-            double b_value = 1 + ( (i + 1) * (double) 1 / NUMBER_OF_LINES);
-            morrisLine.setName(Double.toString(b_value));
-            updateLine.setName(b_value + " updates");
+            morrisLine.setName(Double.toString(B_VALUES[i]));
+            XYChart.Series<Number, Number> lineUpdate = new XYChart.Series<>();
+            lineUpdate.setName(B_VALUES[i] + " updates");
+            morrisLines.add(morrisLine);
+            updateLines.add(lineUpdate);
+            LINE_CHART.getData().add(morrisLine);
+            LINE_CHART.getData().add(lineUpdate);
+        }
+
+        ArrayList<ArrayList<MorrisCounter>> morrisCounters = new ArrayList<>();
+
+        for (int i = 0; i < B_VALUES.length; i++) {
             morrisCounters.add(new ArrayList<>());
             for (int j = 0; j < NUMBER_OF_COUNTERS; j++) {
-                morrisCounters.get(i).add(new MorrisCounter(b_value));
+                morrisCounters.get(i).add(new MorrisCounter(B_VALUES[i]));
             }
-            LINE_CHART.getData().add(morrisLine);
-            LINE_CHART.getData().add(updateLine);
-            morrisLines.add(morrisLine);
-            updateLines.add(updateLine);
         }
-        LINE_CHART.getData().add(basicCounterLine);
+
 
         new AnimationTimer() {
             private long count;
@@ -71,16 +79,14 @@ public class ComparingMorrisCounterBValues extends Application {
                     return;
                 }
 
-
-
                 ArrayList<Long> sums = new ArrayList<>();
                 ArrayList<Long> updates = new ArrayList<>();
-                for (int i = 0; i < NUMBER_OF_LINES; i++) {
+                for (int i = 0; i < B_VALUES.length; i++) {
                     sums.add(0L);
                     updates.add(0L);
                 }
 
-                for (int i = 0; i < NUMBER_OF_LINES; i++) {
+                for (int i = 0; i < B_VALUES.length; i++) {
                     ArrayList<MorrisCounter> mcs = morrisCounters.get(i);
                     for (int j = 0; j < NUMBER_OF_COUNTERS; j++) {
                         MorrisCounter mc = mcs.get(j);
@@ -97,23 +103,26 @@ public class ComparingMorrisCounterBValues extends Application {
 
 
 
-                for (int i = 0; i < NUMBER_OF_LINES; i++) {
+                for (int i = 0; i < B_VALUES.length; i++) {
                     double averageCount = (double) sums.get(i) / NUMBER_OF_COUNTERS;
-                    double averageUpdates = (double) updates.get(i) / NUMBER_OF_COUNTERS;
+                    double averageUpdates = (double) updates.get(i);
                     morrisLines.get(i).getData().add(new XYChart.Data<>(count, averageCount));
                     updateLines.get(i).getData().add(new XYChart.Data<>(count, averageUpdates));
                 }
+
                 basicCounterLine.getData().add(new XYChart.Data<>(count, count));
 
                 count += UPDATES_PER_FRAME;
-
             }
         }.start();
 
         Scene scene = new Scene(LINE_CHART, 800, 600);
         stage.setScene(scene);
         stage.show();
+
     }
+
+
 
 
     public static void main(String[] args) {
