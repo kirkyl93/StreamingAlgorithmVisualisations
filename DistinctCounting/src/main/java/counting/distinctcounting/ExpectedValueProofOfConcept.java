18b6adee815029a67c7f90c12f1ffdc7043cab04
kirkyl93 <l.kirkham@hotmail.com>
2022-08-22 17:48:23 +0100

    Added a distinct counting proof of concept class and a class to measure the median error of Apache Datasketches' implementation of HLL and CPC.

---
+++ DistinctCounting/src/main/java/counting/distinctcounting/ExpectedValueProofOfConcept.java
@@ -0,0 +1,144 @@
+package counting.distinctcounting;
+
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class ExpectedValueProofOfConcept extends Application {
+
+    @Override
+    public void start(Stage stage) {
+
+        Random rand = new Random();
+
+        // Set the k Value of the KMVs
+        final int K_VALUE = 10;
+
+        // Set the number of KMVs we run simultaneously. We can use a higher value to get a better idea of the average
+        // performance of our algorithm.
+        final int NUMBER_OF_KMVS = 5000000;
+
+        // Set the number of distinct items we will count to until the algorithm terminates.
+        final long DISTINCT_COUNT = 1000000;
+
+        // Set the max number that our random number generator can generate. In order for the algorithm to work, this
+        // has to be set higher than the DISTINCT_COUNT number.
+        final long UPPER_LIMIT_NUM_TO_ADD = 150000000;
+
+        // Set the number of updates made to our KMVs before refreshing the graph visualisation. The smaller this is,
+        // the more detail that can be seen in the results. However, it will take the program much longer to arrive at
+        // large count values.
+        final int UPDATES_PER_FRAME = 1;
+
+        // Prepare line charts
+        stage.setTitle("Expected value proof of concept");
+        final NumberAxis distinctItems = new NumberAxis();
+        distinctItems.tickLabelFontProperty().set(Font.font(20));
+        distinctItems.setLabel("Distinct Items");
+        final NumberAxis algorithmEstimate = new NumberAxis();
+        algorithmEstimate.tickLabelFontProperty().set(Font.font(20));
+        algorithmEstimate.setLabel("Algorithm estimate");
+        final LineChart<Number, Number> LINE_CHART_ESTIMATE = new LineChart<>(distinctItems, algorithmEstimate);
+        LINE_CHART_ESTIMATE.setTitle("Expected value proof of concept");
+        LINE_CHART_ESTIMATE.setStyle("-fx-font-size: " + 24 + "px;");
+        LINE_CHART_ESTIMATE.setAnimated(false);
+        LINE_CHART_ESTIMATE.setCreateSymbols(false);
+
+        Stage secondStage = new Stage();
+        secondStage.setTitle("Percentage Error of average of KMV queries");
+        final NumberAxis distinctItems2 = new NumberAxis();
+        distinctItems2.tickLabelFontProperty().set(Font.font(20));
+        distinctItems2.setLabel("Distinct Items");
+        final NumberAxis percentageError = new NumberAxis();
+        percentageError.tickLabelFontProperty().set(Font.font(20));
+        percentageError.setLabel("Percentage error");
+        final LineChart<Number, Number> LINE_CHART_ERROR = new LineChart<>(distinctItems2, percentageError);
+        LINE_CHART_ERROR.setTitle("Percentage Error of average of KMV queries");
+        LINE_CHART_ERROR.setStyle("-fx-font-size: " + 24 + "px;");
+        LINE_CHART_ERROR.setAnimated(false);
+        LINE_CHART_ERROR.setCreateSymbols(false);
+
+        XYChart.Series<Number, Number> trueEstimate = new XYChart.Series<>();
+        trueEstimate.setName("True estimate line");
+        LINE_CHART_ESTIMATE.getData().add(trueEstimate);
+
+        XYChart.Series<Number, Number> kmvsEstimate = new XYChart.Series<>();
+        kmvsEstimate.setName("KMVs estimate");
+        LINE_CHART_ESTIMATE.getData().add(kmvsEstimate);
+
+        XYChart.Series<Number, Number> percentageErrorLine = new XYChart.Series<>();
+        percentageErrorLine.setName("Percentage error");
+        LINE_CHART_ERROR.getData().add(percentageErrorLine);
+
+        ArrayList<PairwiseKMV> kmvs = new ArrayList<>(NUMBER_OF_KMVS);
+        for (int i = 0; i < NUMBER_OF_KMVS; i++) {
+            kmvs.add(new PairwiseKMV(K_VALUE));
+        }
+
+        // Set up basic true distinct count
+        BasicDistinctCountingHash trueDistinctCount = new BasicDistinctCountingHash();
+
+        new AnimationTimer() {
+            private long currentDistinctCount = 0;
+
+            @Override
+            public void handle(long current) {
+
+                // Terminate when our current count is greater than the count limit we set earlier
+                if (currentDistinctCount > DISTINCT_COUNT) {
+                    return;
+                }
+
+                // Generate random numbers to add to KMVs
+                ArrayList<Long> randomNumbersToAdd = new ArrayList<>(UPDATES_PER_FRAME);
+                for (int i = 0; i < UPDATES_PER_FRAME; i++) {
+                    randomNumbersToAdd.add(rand.nextLong(UPPER_LIMIT_NUM_TO_ADD));
+                }
+
+                long estimate = 0;
+
+                for (int i = 0; i < NUMBER_OF_KMVS; i++) {
+                    PairwiseKMV kmv = kmvs.get(i);
+                    estimate += kmv.query();
+                    for (int j = 0; j < UPDATES_PER_FRAME; j++) {
+                        kmv.update(randomNumbersToAdd.get(j));
+                    }
+                }
+
+                double percentageError = 0;
+                double kmvEstimate = (double) estimate / NUMBER_OF_KMVS;
+
+                if (currentDistinctCount > 0) {
+                    percentageError = (kmvEstimate - currentDistinctCount) / currentDistinctCount * 100;
+                }
+
+                for (int i = 0; i < UPDATES_PER_FRAME; i++) {
+                    trueDistinctCount.update(randomNumbersToAdd.get(i));
+                }
+
+                trueEstimate.getData().add(new XYChart.Data<>(currentDistinctCount, currentDistinctCount));
+                kmvsEstimate.getData().add(new XYChart.Data<>(currentDistinctCount, kmvEstimate));
+                percentageErrorLine.getData().add(new XYChart.Data<>(currentDistinctCount, percentageError));
+
+                currentDistinctCount = trueDistinctCount.query();
+            }
+        }.start();
+
+        Scene scene = new Scene(LINE_CHART_ESTIMATE, 800, 600);
+        stage.setScene(scene);
+        stage.show();
+
+        Scene scene2 = new Scene(LINE_CHART_ERROR, 800, 600);
+        secondStage.setScene(scene2);
+        secondStage.show();
+
+    }
+}
---
+++ DistinctCounting/src/main/java/counting/distinctcounting/MedianHLLCPCError.java
@@ -0,0 +1,167 @@
+package counting.distinctcounting;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import org.apache.datasketches.cpc.CpcSketch;
+import org.apache.datasketches.hll.HllSketch;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+public class MedianHLLCPCError extends Application {
+
+    @Override
+    public void start(Stage stage) {
+
+        Random rand = new Random();
+
+        // Set this value between 4 and 21. This controls the sketch memory usage for HLL and CPC.
+        int LG_K_VALUE = 10;
+
+        // Set the number of HLL and CPCs run simultaneously
+        int NUMBER_OF_SKETCHES = 20;
+
+        // Set the max number that our random number generator can generate. In order for the algorithm to work, this
+        // has to be set higher than the DISTINCT_COUNT number.
+        final long UPPER_LIMIT_NUM_TO_ADD = 15000000000L;
+
+        // Set the number of updates made to our sketches before adding results to our graph. The smaller this is,
+        // the more detail that can be seen in the results. However, it will take the program much longer to arrive at
+        // large count values.
+        final int UPDATES_PER_ITERATION = 10000;
+
+        // Set the number of iterations to be run.
+        final int NUMBER_OF_ITERATIONS = 1000;
+
+        // Prepare line chart
+        stage.setTitle("Median and 90th percentile line - CPC and HLL");
+        final NumberAxis distinctItems = new NumberAxis();
+        distinctItems.tickLabelFontProperty().set(Font.font(20));
+        final NumberAxis algorithmEstimate = new NumberAxis();
+        algorithmEstimate.tickLabelFontProperty().set(Font.font(20));
+        distinctItems.setLabel("Distinct Items");
+        algorithmEstimate.setLabel("Algorithm percentage error");
+        final LineChart<Number, Number> LINE_CHART = new LineChart<>(distinctItems, algorithmEstimate);
+        LINE_CHART.setStyle("-fx-font-size: " + 24 + "px;");
+        LINE_CHART.setTitle("Median and 90th percentile line - CPC and HLL");
+        LINE_CHART.setAnimated(false);
+        LINE_CHART.setCreateSymbols(false);
+
+        // Set up lines for median and 90th percentile error for both HLL and CPC
+        XYChart.Series<Number, Number> HLLMedianLine = new XYChart.Series<>();
+        HLLMedianLine.setName("HLL Median Line");
+        XYChart.Series<Number, Number> HLLPercentile90Line = new XYChart.Series<>();
+        HLLPercentile90Line.setName("HLL 90th Percentile Line");
+
+
+        XYChart.Series<Number, Number> CPCMedianLine = new XYChart.Series<>();
+        CPCMedianLine.setName("CPC Median Line");
+        XYChart.Series<Number, Number> CPCPercentile90Line = new XYChart.Series<>();
+        CPCPercentile90Line.setName("CPC 90th Percentile Line");
+
+        // Set the first point on both lines to (0,0)
+        HLLMedianLine.getData().add(new XYChart.Data<>(0, 0));
+        HLLPercentile90Line.getData().add(new XYChart.Data<>(0, 0));
+
+        CPCMedianLine.getData().add(new XYChart.Data<>(0, 0));
+        CPCPercentile90Line.getData().add(new XYChart.Data<>(0, 0));
+
+        // Set up arraylists to store absolute errors of our sketches
+        ArrayList<ArrayList<Double>> allHLLAbsoluteErrors = new ArrayList<>(NUMBER_OF_ITERATIONS);
+        ArrayList<ArrayList<Double>> allCPCAbsoluteErrors = new ArrayList<>(NUMBER_OF_ITERATIONS);
+        ArrayList<Long> trueDistinctCounts = new ArrayList<>(NUMBER_OF_ITERATIONS);
+        for (int i = 0; i < NUMBER_OF_ITERATIONS; i++) {
+            allHLLAbsoluteErrors.add(new ArrayList<>(NUMBER_OF_SKETCHES));
+            allCPCAbsoluteErrors.add(new ArrayList<>(NUMBER_OF_SKETCHES));
+            trueDistinctCounts.add(0L);
+        }
+
+        // Set the median and 90th percentile position in array
+        int medianValue = NUMBER_OF_SKETCHES / 2;
+        int percentile90Value = (NUMBER_OF_SKETCHES / 10) * 9;
+
+        long hllBytesUsed = 0L;
+        long cpcBytesUsed = 0L;
+
+        for (int i = 0; i < NUMBER_OF_SKETCHES; i++) {
+
+            // Create a new sketch
+            HllSketch hll = new HllSketch(LG_K_VALUE);
+            CpcSketch cpc = new CpcSketch(LG_K_VALUE);
+            BasicDistinctCountingHash trueCount = new BasicDistinctCountingHash();
+
+            // Update hll, cpc and trueCount with random numbers
+            for (int j = 0; j < NUMBER_OF_ITERATIONS; j++) {
+                for (int k = 0; k < UPDATES_PER_ITERATION; k++) {
+                    long randNum = rand.nextLong(UPPER_LIMIT_NUM_TO_ADD);
+                    hll.update(randNum);
+                    cpc.update(randNum);
+                    trueCount.update(randNum);
+                }
+
+                // Store absolute errors in previously created arraylist
+                allHLLAbsoluteErrors.get(j).add(Math.abs(hll.getEstimate() - trueCount.query()));
+                allCPCAbsoluteErrors.get(j).add(Math.abs(cpc.getEstimate() - trueCount.query()));
+                trueDistinctCounts.set(j, trueDistinctCounts.get(j) + trueCount.query());
+            }
+
+            hllBytesUsed += hll.getCompactSerializationBytes();
+            cpcBytesUsed += cpc.toByteArray().length;
+            System.out.println("Made it through " + i + " sketch");
+            System.out.println("HLL is: " + hll.getCompactSerializationBytes());
+            System.out.println("CPC is: " + cpc.toByteArray().length);
+        }
+
+
+        for (int i = 0; i < NUMBER_OF_ITERATIONS; i++) {
+            //Retrieve absolute errors and average true count
+            ArrayList<Double> hllAbsoluteErrors = allHLLAbsoluteErrors.get(i);
+            ArrayList<Double> cpcAbsoluteErrors = allCPCAbsoluteErrors.get(i);
+            double trueCount = (double) trueDistinctCounts.get(i) / NUMBER_OF_SKETCHES;
+
+            // Sort errors and find median and 90th percentile errors for HLL and CPC
+            Collections.sort(hllAbsoluteErrors);
+            Collections.sort(cpcAbsoluteErrors);
+
+            double hllMedianPercentageError = hllAbsoluteErrors.get(medianValue) / trueCount * 100;
+            double hllPercentile90PercentageError = hllAbsoluteErrors.get(percentile90Value) / trueCount * 100;
+            double cpcMedianPercentageError = cpcAbsoluteErrors.get(medianValue) / trueCount * 100;
+            double cpcPercentile90PercentageError = cpcAbsoluteErrors.get(percentile90Value) / trueCount * 100;
+
+            //Update lines
+            HLLMedianLine.getData().add(new XYChart.Data<>(trueCount, hllMedianPercentageError));
+            HLLPercentile90Line.getData().add(new XYChart.Data<>(trueCount, hllPercentile90PercentageError));
+
+            CPCMedianLine.getData().add(new XYChart.Data<>(trueCount, cpcMedianPercentageError));
+            CPCPercentile90Line.getData().add(new XYChart.Data<>(trueCount, cpcPercentile90PercentageError));
+
+            System.out.println("Added one of those points on line you know what I mean?");
+
+        }
+
+        System.out.println("HLL size is: " + (double) hllBytesUsed / NUMBER_OF_SKETCHES);
+        System.out.println("CPC size is: " + (double) cpcBytesUsed / NUMBER_OF_SKETCHES);
+
+        // Prepare scene
+        LINE_CHART.getData().add(HLLMedianLine);
+        LINE_CHART.getData().add(HLLPercentile90Line);
+
+        LINE_CHART.getData().add(CPCMedianLine);
+        LINE_CHART.getData().add(CPCPercentile90Line);
+
+
+        Scene scene = new Scene(LINE_CHART, 800, 600);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
+
