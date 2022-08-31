b356cbe241e177c5a1fb5616048bd73222c1a429
kirkyl93 <l.kirkham@hotmail.com>
2022-08-31 21:36:14 +0100

    Added the hash function class and added comments to the count min sketch original class

--- FrequencyCounting/src/main/java/counting/frequencycounting/CountMinSketchOriginal.java
+++ FrequencyCounting/src/main/java/counting/frequencycounting/CountMinSketchOriginal.java
@@ -2,20 +2,23 @@ package counting.frequencycounting;
 
 public class CountMinSketchOriginal extends CountMinSketch {
 
-
+    // Basic constructor
     public CountMinSketchOriginal(int d, int t) {
         super(d, t);
     }
 
+    // This constructor is used when we want to create a new sketch with the same parameters as an existing sketch
     public CountMinSketchOriginal(CountMinSketchOriginal cms) {
     super(cms);
     }
 
+    // This constructor is used to create a new merged count min sketch original
     public CountMinSketchOriginal(CountMinSketchOriginal cms, long[][] matrix) {
         super(cms, matrix);
     }
 
 
+    // Algorithm 4.2
     public void update(long item, long weight) {
         for (int i = 0; i < d; i++) {
             int hashValue = (int) (hashFunctions[i].hashFunction(item) % t);
---
+++ FrequencyCounting/src/main/java/counting/frequencycounting/Hash.java
@@ -0,0 +1,37 @@
+package counting.frequencycounting;
+
+import java.util.Random;
+
+/** An abstract class for k-wise independent hash functions. Child classes are required to implement the hashFunction
+ * method
+ *
+ */
+
+public abstract class Hash {
+
+    private final long p = 887213367811L;
+    private final long a;
+    private final long b;
+
+    // The constructor generates two random parameters between 0 and p
+    public Hash() {
+        Random rand = new Random();
+        this.a = rand.nextLong(p);
+        this.b = rand.nextLong(p);
+    }
+
+    public abstract long hashFunction (long x);
+
+    public long getPrime() {
+        return p;
+    }
+
+    public long getA() {
+        return a;
+    }
+
+    public long getB() {
+        return b;
+    }
+
+}
